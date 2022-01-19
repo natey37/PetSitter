83624edf0ab55aa3de54389d84ab7c4f468c1c96
Nathan Brickett <natey37@gmail.com>
2022-01-18 21:35:50 -0800

    create profile model schema

---
+++ server/models/Profile.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  gender: { type: String },
+  birthdate: { type: Date },
+  email: { type: String },
+  phoneNumber: { type: String },
+  address: { type: String },
+  description: { type: String },
+  availability: {
+    type: Map,
+    of: new Schema({
+        startDate: { type: Date },
+        endDate: { type: Date },
+        availableTimes: [{ startTime: { type: String }, endTime: { type: String}}]
+    })
+  },
+  profilePhoto: { type: String },
+  paymentOptions: { type: [String] },
+  userID: {
+    type: Schema.Types.ObjectId, 
+    ref: 'User' 
+  },
+  hourlyRate: { type: Number }
+});
+
+module.exports = Profile = mongoose.model("profile", profileSchema);
